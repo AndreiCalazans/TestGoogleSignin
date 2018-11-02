304ecf0327e8d3c1d80928f3219e0f7edd7871b8
AndreiCalazans <andreixoc@hotmail.com>
2018-11-02 17:46:42 -0300

    step 4: Add config file with iosClientId and webClientId + code to run the app

---
+++ config.js
@@ -0,0 +1,4 @@
+export default {
+  iosClientId: '179212921806-o3o5eoponn8b20r9nvvopn3qv2403fuc.apps.googleusercontent.com',
+  webClientId: '179212921806-7pnl5mlp3es9f7neqhk71in3ibb4uavb.apps.googleusercontent.com'
+}
