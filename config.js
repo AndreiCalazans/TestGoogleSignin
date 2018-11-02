e8cbfe01b095859b55d9a1a10a843abf6c3305de
AndreiCalazans <andreixoc@hotmail.com>
2018-11-02 18:23:13 -0300

    step 4: include code to handle sign in and add iosClientId

---
+++ config.js
@@ -0,0 +1,4 @@
+export default {
+  iosClientId: '179212921806-qc96m8kb4snpodn5keklpu01qcko7bf4.apps.googleusercontent.com',
+  webClientId: '179212921806-7pnl5mlp3es9f7neqhk71in3ibb4uavb.apps.googleusercontent.com'
+}
