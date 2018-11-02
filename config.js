da50b4c226f2adec68f2a71d365dbab1089ef3c5
AndreiCalazans <andreixoc@hotmail.com>
2018-11-02 17:59:44 -0300

    step 5: Add GoogleService-Info.plist to project via xcode and update iosClientId

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export default {
-  iosClientId: '179212921806-o3o5eoponn8b20r9nvvopn3qv2403fuc.apps.googleusercontent.com',
+  iosClientId: '179212921806-qc96m8kb4snpodn5keklpu01qcko7bf4.apps.googleusercontent.com',
   webClientId: '179212921806-7pnl5mlp3es9f7neqhk71in3ibb4uavb.apps.googleusercontent.com'
 }
